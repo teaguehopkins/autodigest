8a28e76cded86d4d9ac4bdec4422785d822d5eba
Teague Hopkins <teague@teaguehopkins.com>
2013-06-11 17:10:49 -0400

    Gets article title using Mechanize

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,10 +7,15 @@ class Article < ActiveRecord::Base
   private
 
   	def set_title
-  		@key="1c6012863bac4e67bba0c2b62e591fbe"
+  		#@api= Embedly::API.new
+  		#@extracted_article = api.oembed :url => self.link
+  		#@key="1c6012863bac4e67bba0c2b62e591fbe"
   		#@extracted_article = http://api.embed.ly/1/extract?key=:key&url=:url&format=:format
   		#@extracted_article = http://api.embed.ly/1/extract?key=1c6012863bac4e67bba0c2b62e591fbe&url=www.teaguehopkins.com&format=json
-  		self.title = "Default Title"
+  		agent=Mechanize.new # Creates a new Mechanize Object
+agent.get(self.link) # This fetches the page given as parameter
+ # This will return the title of the page
+  		self.title = agent.page.title
   		#self.title = @extracted_article.title
   	end
 end
