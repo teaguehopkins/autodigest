d34cfb419306503b388be33fd5748ebca45beae8
Teague Hopkins <teague@teaguehopkins.com>
2013-06-12 17:29:07 -0400

    Moves critical email CSS to internal style sheet and fixes URLs
    * Moves basic CSS to application layout.
    * Uses end URL instead of original URL
    * Slices '?' and anything after it off of URL

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,7 +9,13 @@ class Article < ActiveRecord::Base
   	def set_data
   		api= Embedly::API.new :key => '1c6012863bac4e67bba0c2b62e591fbe'
   		obj = api.oembed :url => self.link
-  		self.title = obj[0].title
+      @url = obj[0].url
+  		if @url.include? '?'
+        self.link = @url.slice(0..(@url.index('?')-1))
+      else
+        self.link = @url
+      end
+      self.title = obj[0].title
   		self.summary = obj[0].description
   		self.author = obj[0].author_name
       self.thumbnail_url = obj[0].thumbnail_url
