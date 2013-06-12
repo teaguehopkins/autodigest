27384a7eaec05e1aa5bd79c12c2d9598b180943c
Teague Hopkins <teague@teaguehopkins.com>
2013-06-12 18:19:44 -0400

    Fix article edits and Scroll to bottom
    *Changes nils to empty strings.
    *Scrolls article add page to bottom on load.

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,21 +10,24 @@ class Article < ActiveRecord::Base
   		api= Embedly::API.new :key => '1c6012863bac4e67bba0c2b62e591fbe'
   		obj = api.oembed :url => self.link
       @url = obj[0].url
+      if @url==nil
+        @url=self.link
+      end
   		if @url.include? '?'
         self.link = @url.slice(0..(@url.index('?')-1))
       else
         self.link = @url
       end
-      if self.title==nil
+      if self.title==""
         self.title = obj[0].title
       end
-      if self.summary==nil
+      if self.summary==""
   		  self.summary = obj[0].description
       end
-  		if self.author==nil
+  		if self.author==""
         self.author = obj[0].author_name
       end
-      if self.thumbnail_url==nil
+      if self.thumbnail_url==""
         self.thumbnail_url = obj[0].thumbnail_url
       end  	
     end
